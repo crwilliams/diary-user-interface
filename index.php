452a80fce977844d0b281f4659cdb5894641361b
Colin Williams <github@crwilliams.co.uk>
2012-03-04 22:41:09 +0000

    Added footer links.

--- index.php
+++ index.php
@@ -1,4 +1,25 @@
 <?php
 require_once('inc/functions.php');
-renderPage('Title', 'Description', 'Header', 'Content', 'Footer');
+
+/* Configuration */
+
+$pagetitle = 'Calendar';
+$pagedescription = 'A Calendar Viewer';
+
+$content = "";
+
+$footerlinks[] = array("Link 1", "http://example.com");
+$footerlinks[] = array("Link 2", "http://example.com");
+$footerlinks[] = array("Link 3", "http://example.com");
+$footerlinks[] = array("Link 4", "http://example.com");
+
+/* End of configuration */
+
+$footer = "<ul>";
+foreach($footerlinks as $link) {
+	$footer .= "<li><a href='".$link[1]."'>".$link[0]."</a></li>";
+}
+$footer .= "</ul>";
+
+renderPage($pagetitle, $pagedescription, '', $content, $footer);
 ?>
