d9c2230ba20eb363405e736af19b1da025d1190e
Colin Williams <github@crwilliams.co.uk>
2012-03-05 01:40:30 +0000

    Improved styling

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ $content = "";
 $content .= "<div class='box sidebox' id='calendar-search'><div class='header'>Search by date</div><div class='content'>".mklist()."</div></div>";
 $content .= "<div class='box sidebox' id='department-search'><div class='header'>Search by department</div><div class='content'>".mklist()."</div></div>";
 $content .= "<div class='box sidebox' id='location-search'><div class='header'>Search by location</div><div class='content'>".mklist()."</div></div>";
-$content .= "<div class='box mainbox' id='main'><div class='header'>Events Calendar<a class='right'>View all events</a></div><div class='content'>".mklist()."</div></div>";
+$content .= "<div class='box mainbox' id='main'><div class='header'><h1>Events Calendar</h1><a class='right'>View all events</a></div><div class='content'>".mklist()."</div></div>";
 
 $footerlinks[] = array("Link 1", "http://example.com");
 $footerlinks[] = array("Link 2", "http://example.com");
