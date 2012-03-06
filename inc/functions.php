89409587137c8cafdef56f438e28e4da925dca39
Colin Williams <github@crwilliams.co.uk>
2012-03-06 00:27:07 +0000

    Removed X-UA-Compatible to pass W3C validation.

--- inc/functions.php
+++ inc/functions.php
@@ -12,10 +12,6 @@ function renderPage($title, $description, $header, $content, $footer)
 <head>
   <meta charset="utf-8">
 
-  <!-- Use the .htaccess and remove these lines to avoid edge case issues.
-       More info: h5bp.com/i/378 -->
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-
   <title><?php echo $title ?></title>
   <meta name="description" content="<?php echo $description ?>">
 
