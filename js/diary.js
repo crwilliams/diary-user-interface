6cdc8b2e4adb5c712d82ddf97c0ff2e550e79768
Colin Williams <github@crwilliams.co.uk>
2012-03-10 17:52:48 +0000

    Handling of hash change and related updates.

--- js/diary.js
+++ js/diary.js
@@ -7,6 +7,83 @@ function unavailable(date) {
   }
 }
 
+var selDate = '';
+var selOrg = '';
+var selPlace = '';
+var fullhash = '';
+
+function processHash() {
+	var hashparts = fullhash.substring(1).split('/');
+	for(var i = 0; i < hashparts.length; i++) {
+		console.log(hashparts[i]);
+		if(hashparts[i].substring(0, 4) == 'org-') {
+			selOrg = hashparts[i];
+			console.log('org: ' + selOrg);
+		}
+		else if(hashparts[i].substring(0, 5) == 'site-') {
+			selPlace = hashparts[i];
+			console.log('place: ' + selPlace);
+		}
+		else if(hashparts[i].length == 10 && hashparts[i][2] == '-' && hashparts[i][5] == '-') {
+			selDate = hashparts[i];
+			console.log('date: ' + selDate);
+		}
+	}
+	setHash();
+	
+}
+
+function setDate(str) {
+	if(str == '')
+		selDate = '';
+	else
+		selDate = str.substring(3,5) + '-' + str.substring(0,2) + '-' + str.substring(6,10);
+	setHash();
+}
+
+function setOrg(str) {
+	selOrg = str;
+	setHash();
+}
+
+function setPlace(str) {
+	selPlace = str;
+	setHash();
+}
+
+function setHash() {
+	var str = "";
+	if(selOrg != '')
+		str += '/' + selOrg;
+	if(selPlace != '')
+		str += '/' + selPlace;
+	if(selDate != '')
+		str += '/' + selDate;
+	window.location.hash = str.substring(1);
+}
+
 $(function() {
-	$( "#calendar-search div.content" ).datepicker({ beforeShowDay: unavailable, dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'], firstDay: 1 });
+	$( "#calendar-search div.content" ).datepicker({ beforeShowDay: unavailable, dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'], firstDay: 1, onSelect: function(dateText, inst) { setDate(dateText); } });
+	$( "ul#org a" ).click(function(e) {
+		setOrg(e.target.id);
+		return false;
+	});
+	$( "ul#place a" ).click(function(e) {
+		setPlace(e.target.id);
+		return false;
+	});
+	$( "#alldates" ).click(function(e) {
+		setDate('');
+		return false;
+	});
 });
+
+$(window).hashchange( function(){
+	if(location.hash != fullhash)
+	{
+		fullhash = location.hash;
+		processHash();
+	}
+});
+
+$(window).hashchange();
