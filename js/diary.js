6a703736b449cbf858484c8d1033e5d5b8c2c963
Colin Williams <github@crwilliams.co.uk>
2012-03-10 14:49:24 +0000

    Added styling of calendar.

--- js/diary.js
+++ js/diary.js
@@ -1,3 +1,14 @@
+var availableDates = ["9-3-2012","14-3-2012","15-3-2012"];
+
+function unavailable(date) {
+  dmy = date.getDate() + "-" + (date.getMonth()+1) + "-" + date.getFullYear();
+  if ($.inArray(dmy, availableDates) == -1) {
+    return [false,"","Unavailable"];
+  } else {
+    return [true, ""];
+  }
+}
+
 $(function() {
-		$( "#calendar-search/div.content" ).datepicker();
+	$( "#calendar-search div.content" ).datepicker({ beforeShowDay: unavailable, dayNamesMin: ['S', 'M', 'T', 'W', 'T', 'F', 'S'], firstDay: 1 });
 });
