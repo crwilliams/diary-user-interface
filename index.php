413c7987b63e76a544ccd8744dd8e5a7738f3465
Colin Williams <github@crwilliams.co.uk>
2012-03-11 14:24:14 +0000

    Made 'View full calendar' link visible on homepage only.

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ $graph = loadGraph($organisers, $places, $events);
 
 $content = "";
 $content .= "<script>var availableDates = []</script>";
-$content .= "<div class='box sidebox' id='calendar-search'><div class='header'>Search by date</div><div class='content'><a href='#' id='alldates' class='view-events'>View full calendar</a></div></div>";
+$content .= "<div class='box sidebox' id='calendar-search'><div class='header'>Search by date</div><div class='content'><a href='#' id='alldates' class='view-events homepage-only'>View full calendar</a></div></div>";
 $content .= "<div class='box sidebox' id='department-search'><div class='header'>Search by department</div><div class='content'>".getOptionTree($organisers, 'org', 'All departments', 'getOrganisationTreeOptions', $graph)."</div></div>";
 $content .= "<div class='box sidebox' id='location-search'><div class='header'>Search by location</div><div class='content'>".getOptionTree($places, 'place', 'All locations')."</div></div>";
 $content .= "<div class='box mainbox' id='main'><div class='header'><h1>Events Calendar</h1><a id='view-all' class='view-events right'>View all events</a></div><div class='content'><b class='homepage-only'>Upcoming Events</b>".getEventListings($events)."<a href='#' class='view-events right homepage-only'>View all events</a></div></div>";
